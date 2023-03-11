96863aca96ef0f8a5d4f34b0118e580d3e8306e0
Light <coolkevin011@gmail.com>
2023-03-11 20:46:08 +0530

    doc(): add some doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+//! # pascal_string
+//! `pascal_string` exports a procedural macro to converta n identifier to a string literal in pascal case
+
 use inflector::Inflector;
 use proc_macro::{Literal, TokenStream, TokenTree};
 
+/// Convert an identifier to a pascal case string literal
+/// e.g. `my_rusty_identifier` becomes `MyRustyIdentifier`
 #[proc_macro]
 pub fn pascal_string(stream: TokenStream) -> TokenStream {
     stream
